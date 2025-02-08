eab9ab2e5a7cf0771ee55d0230eb75ec7a9db657
SafouaneHaddadi <safouane.haddadi@condorcet.be>
2025-02-08 16:04:58 +0100

    staging workflow

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,38 +1,48 @@
 module "eks" {
+  # Source du module EKS, provenant du registre Terraform
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "19.19.1"  # Version du module à utiliser
 
+  # Nom du cluster, défini par la variable locale
   cluster_name    = local.cluster_name
+  
+  # Version de Kubernetes à déployer sur le cluster EKS
   cluster_version = "1.27"
 
+  # Identifiant du VPC où le cluster sera déployé
   vpc_id                         = module.vpc.vpc_id
+  
+  # Sous-réseaux privés où les nœuds du cluster seront déployés
   subnet_ids                     = module.vpc.private_subnets
+  
+  # Activation de l'accès public à l'endpoint du cluster EKS
   cluster_endpoint_public_access = true
 
+  # Configuration par défaut pour les groupes de nœuds gérés par EKS
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
+    ami_type = "AL2_x86_64"  # Type d'AMI à utiliser pour les nœuds (Amazon Linux 2)
   }
 
+  # Définition des groupes de nœuds gérés par EKS
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
+    one = { #autoscaling group information pour le grp de noeuds
+      name = "node-group-1"  # Nom du premier groupe de nœuds
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.small"]  # Type d'instance à utiliser
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size     = 1  # Taille minimale du groupe de nœuds
+      max_size     = 3  # Taille maximale du groupe de nœuds
+      desired_size = 2  # Taille désirée du groupe de nœuds
     }
 
     two = {
-      name = "node-group-2"
+      name = "node-group-2"  # Nom du deuxième groupe de nœuds
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.small"]  # Type d'instance à utiliser
 
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size     = 1  # Taille minimale du groupe de nœuds
+      max_size     = 2  # Taille maximale du groupe de nœuds
+      desired_size = 1  # Taille désirée du groupe de nœuds
     }
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,21 @@
 variable "region" {
+  # Description de la variable, ici elle représente la région AWS à utiliser
   description = "AWS region"
+  
+  # Type de la variable, spécifiant qu'il s'agit d'une chaîne de caractères
   type        = string
+  
+  # Valeur par défaut de la variable, utilisée si aucune autre valeur n'est fournie
   default     = "us-east-2"
 }
 
 variable "clusterName" {
+  # Description de la variable, ici elle représente le nom du cluster EKS à créer
   description = "Name of the EKS cluster"
+  
+  # Type de la variable, spécifiant qu'il s'agit d'une chaîne de caractères
   type        = string
-  default     = "kitops-eks"
+  
+  # Valeur par défaut de la variable, utilisée si aucune autre valeur n'est fournie
+  default     = "vprofile-eks"
 }
